e0dbc8a04dce5b4594aa1d0181c639f03f170a55
aanoora sehar <93789339+anooravk@users.noreply.github.com>
2023-07-07 22:37:25 +0500

    Delete controllers directory

--- controllers/admin/adminController.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once '../../models/database_connect.php';
-session_start();
-if(!isset($_SESSION['loggedinuser']))
-{
-    header("Location:../../views/Login.php");
-}
-?>
--- controllers/superadmin/superadminController.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    if(!isset($_SESSION['loggedinuser']))
-	{
-		header("Location:../../views/Login.php");
-    }
-?>
